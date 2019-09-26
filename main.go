552fda43ad80a934d8299df692b7b9a49f7df12b
Huascar Baptista <huascar.baptista@rappi.com>
2019-09-26 09:19:57 -0500

    First commit add label to Jira Issue

---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"encoding/base64"
+	"fmt"
+	"github.com/HuascarBaptista/bitrise-step-update-label/jira"
+	"github.com/bitrise-io/go-utils/log"
+	"github.com/bitrise-tools/go-steputils/stepconf"
+	"os"
+	"strings"
+)
+
+// Config ...
+type Config struct {
+	UserName  string `env:"user_name,required"`
+	APIToken  string `env:"api_token,required"`
+	BaseURL   string `env:"base_url,required"`
+	IssueKeys string `env:"jira_tickets,required"`
+	AdditionalLabel   string `env:"label"`
+}
+
+func main() {
+	var cfg Config
+	if err := stepconf.Parse(&cfg); err != nil {
+		failf("Issue with input: %s", err)
+	}
+
+	stepconf.Print(cfg)
+	fmt.Println()
+
+	encodedToken := generateBase64APIToken(cfg.UserName, cfg.APIToken)
+	client := jira.NewClient(encodedToken, cfg.BaseURL)
+	issueKeys := strings.Split(cfg.IssueKeys, `|`)
+
+	var comments []jira.Comment
+	for _, issueKey := range issueKeys {
+		comments = append(comments, jira.Comment{IssuKey: issueKey})
+	}
+
+	if err := client.PostIssueAddLabel(comments, cfg.AdditionalLabel); err != nil {
+		failf("Posting comments failed with error: %s", err)
+	}
+	os.Exit(0)
+}
+
+func generateBase64APIToken(userName string, apiToken string) string {
+	v := userName + `:` + apiToken
+	return base64.StdEncoding.EncodeToString([]byte(v))
+}
+
+func failf(format string, v ...interface{}) {
+	log.Errorf(format, v...)
+	os.Exit(1)
+}
